ed0c8a0607fe2fa49f8729b3d82b45fa8f809a79
faveee <abatangift11@gmail.com>
2022-12-22 10:03:55 +0100

    Newly Created
    description

---
+++ src/Components/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+    return(
+<div className="container">
+    <h1 className="full-name">Abatan Favour</h1>
+    <h4 className="job-position">Frontend Developer</h4>
+    <h3 className="contact-box">Email</h3>
+    <h3 className="contact-box">Linkedln</h3>
+</div>
+    )
+}
+export default Info
---
+++ src/Components/Interests.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Interests() {
+    return(
+<div className="interest-section">
+    <h1 className="interest-title">Interests</h1>
+    <p className="interest-paragraph">Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+    </div>
+    )
+}
+export default Interests
